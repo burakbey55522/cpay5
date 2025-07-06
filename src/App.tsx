760cd4cbaaf03ef0b49a9c45e7729b25b733dbe2
burakbey55522 <burakbey55522@gmail.com>
2025-07-06 15:00:54 +0300

    Add How It Works Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import HowItWorks from './components/HowItWorks';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,6 +10,7 @@ function App() {
     <div className="min-h-screen bg-white">
       <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <Hero />
+      <HowItWorks />
     </div>
   );
 }
